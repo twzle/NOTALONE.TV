900b3d09c22033081e8f3a440f1356cba10280a4
Кирилл Мезенев <kamezenev@miem.hse.ru>
2021-02-15 01:45:11 +0300

    "Rooms Fragment Added"

--- app/src/main/java/com/example/myapplication/RoomsFragment.java
+++ app/src/main/java/com/example/myapplication/RoomsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -12,7 +13,11 @@ import androidx.navigation.fragment.NavHostFragment;
 public class RoomsFragment extends Fragment {
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_rooms, container, false);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_rooms, container, false);
+        TextView mTextView = (TextView) view.findViewById(R.id.no_rooms);
+        mTextView.setVisibility(View.GONE);
+        return view;
     }
 }
+
